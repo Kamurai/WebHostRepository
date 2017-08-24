5b37490651aab62c7cf37c8731c58d31648b1b7c
ChristopherKemerait <Christopher.Kemerait@yahoo.com>
2017-08-23 23:17:18 -0400

    20150327a backup

--- Apache/htdocs/info.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$navHeader = "Navigation";
-	$infoHeader = "Information";
-	$infoContent = "";
-	
-	$footer = "© Copyright 2012 All rights reserved<br>
-				House That Kamurai Built<br>";
-	$management = "Website managed by Kamurai.";
-?>
